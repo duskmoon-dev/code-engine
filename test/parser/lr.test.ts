131a456080f16bf901dd598f9c0dc9735ee38541
Jonathan Gao <gsmlg.com@gmail.com>
2026-03-25 16:20:56 +0800

    test: expand LR parser tests with multi-language and tree traversal coverage

--- test/parser/lr.test.ts
+++ test/parser/lr.test.ts
@@ -1,6 +1,8 @@
 import { describe, it, expect } from "bun:test";
 import { LRParser, ExternalTokenizer, ContextTracker, Stack } from "../../src/parser/lr/index";
 import { pythonLanguage } from "../../src/lang/python/index";
+import { javascriptLanguage } from "../../src/lang/javascript/index";
+import { rustLanguage } from "../../src/lang/rust/index";
 
 describe("LR Parser module", () => {
   describe("exports", () => {
@@ -36,6 +38,35 @@ describe("LR Parser module", () => {
     it("LRParser has topRules", () => {
       const parser = pythonLanguage.parser as LRParser;
       expect(parser.topRules).toBeDefined();
+    });
+
+    it("LRParser.parse returns an incremental tree", () => {
+      const tree = pythonLanguage.parser.parse("for x in range(10): print(x)");
+      expect(tree.length).toBeGreaterThan(0);
+      expect(tree.type.isTop).toBe(true);
+    });
+
+    it("tree cursor can traverse nodes", () => {
+      const tree = pythonLanguage.parser.parse("x = 1");
+      const cursor = tree.cursor();
+      let nodeCount = 0;
+      do {
+        nodeCount++;
+      } while (cursor.next() && nodeCount < 100);
+      expect(nodeCount).toBeGreaterThan(1);
+    });
+
+    it("javascript language parser is also an LRParser", () => {
+      expect(javascriptLanguage.parser instanceof LRParser).toBe(true);
+    });
+
+    it("rust language parser is also an LRParser", () => {
+      expect(rustLanguage.parser instanceof LRParser).toBe(true);
+    });
+
+    it("parsers for different languages are distinct instances", () => {
+      expect(pythonLanguage.parser).not.toBe(javascriptLanguage.parser);
+      expect(pythonLanguage.parser).not.toBe(rustLanguage.parser);
     });
   });
 });
